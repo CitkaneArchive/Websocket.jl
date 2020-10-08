3e9cc630d7e58be634c9e80600342cf5a12ef11a
Michael Jonker <michael@openpoint.ie>
2020-10-08 10:41:41 +0100

    github documenter

--- docs/make.jl
+++ docs/make.jl
@@ -25,6 +25,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/citkane/Websocket.jl.git"
+)
